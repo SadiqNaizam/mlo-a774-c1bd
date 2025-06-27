9eda7de19b0af0a8c8d778d467c4cfed5344586e
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-27 22:31:41 +0530

    Updated code files via regeneration: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
+import { ThemeProvider } from "./contexts/ThemeContext";
 import ThemeCustomizer from "./pages/ThemeCustomizer";
 import NotFound from "./pages/NotFound";
 
@@ -12,23 +12,25 @@ const queryClient = new QueryClient();
 
 
 const App = () => (
-<QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-        <Routes>
+<ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+          <Routes>
 
 
-          <Route path="/" element={<ThemeCustomizer />} />
-          {/* catch-all */}
-          <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<ThemeCustomizer />} />
+            {/* catch-all */}
+            <Route path="*" element={<NotFound />} />
 
 
-        </Routes>
-    </BrowserRouter>
-    </TooltipProvider>
-</QueryClientProvider>
+          </Routes>
+      </BrowserRouter>
+      </TooltipProvider>
+  </QueryClientProvider>
+</ThemeProvider>
 );
 
 export default App;
